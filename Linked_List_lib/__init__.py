54c4b680cf378261a15770ccecb1f32e8cb860a6
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-12-21 19:15:55 -0600

    Update

---
+++ Linked_List_lib/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Author: Daniel Yang <daniel.yj.yang@gmail.com>
+#
+# License: BSD 3-Clause License
+
+
+from .__about__ import (
+    __version__,
+    __license__,
+)
+
+
+# this is for "from <package_name> import *"
+__all__ = [,]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Linked_List_lib
-Library for Studying Linked List
